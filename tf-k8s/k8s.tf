321f681e80c3c513c8ae5b77d4df150e933a9a3a
Radim Daniel Panek <rdpanek@gmail.cz>
2020-05-04 13:25:52 +0200

    tf examples for droplets and k8s cluster

---
+++ tf-k8s/k8s.tf
@@ -0,0 +1,16 @@
+resource "digitalocean_kubernetes_cluster" "tab" {
+
+  name     = "tabk8s"
+  region   = "fra1"
+  version = "1.16.6-do.2"
+  node_pool {
+    name       = "tabpool"
+    size       = "s-2vcpu-4gb"
+    node_count = 1
+  }
+  tags    = ["tab"]
+
+  provisioner "local-exec" {
+    command = "doctl kubernetes cluster kubeconfig save tabk8s"
+  }
+}
