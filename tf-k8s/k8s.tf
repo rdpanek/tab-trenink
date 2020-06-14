f52ebfaf4911526c49405ef45b3ed6fe0c71baec
Radim Daniel Panek <rdpanek@gmail.cz>
2020-06-14 13:32:42 +0200

    tf-k8s/k8s.tf: up version of k8s

--- tf-k8s/k8s.tf
+++ tf-k8s/k8s.tf
@@ -2,7 +2,7 @@ resource "digitalocean_kubernetes_cluster" "tab" {
 
   name     = "tabk8s"
   region   = "fra1"
-  version = "1.16.6-do.2"
+  version = "1.16.8-do.0"
   node_pool {
     name       = "tabpool"
     size       = "s-2vcpu-4gb"
