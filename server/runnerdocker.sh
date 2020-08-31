a69db176d28f716571d57d4dd382c6ffc4511f03
Radim Daniel Panek <rdpanek@gmail.cz>
2020-08-31 23:31:52 +0200

    server/runnerdocker.sh: updated canarytrace image

--- server/runnerdocker.sh
+++ server/runnerdocker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#Linux only
-LOCAL_IP=$(hostname -I | awk '{print $1}')
+# create net
+docker network create canary
 
 for i in `seq 1 1`;
 do
@@ -9,11 +9,11 @@ echo "otoceni: ${i}"
 
 docker kill selen
 docker rm selen
-docker run --name selen -d -p 5902:5900 -p 4444:4444 -p 0.0.0.0:9222:9222 -v /dev/shm:/dev/shm selenium/standalone-chrome-debug:3.141.59-titanium
+docker run --name selen --net canary -d -p 5902:5900 -p 4444:4444 -p 0.0.0.0:9222:9222 -v /dev/shm:/dev/shm selenium/standalone-chrome-debug:3.141.59-20200730
 
 for y in `seq 1 20`;
 do
-    IS_SELEN_LIVE=$(curl -I -X GET http://${LOCAL_IP}:4444/ 2>/dev/null | head -n 1 | cut -d$' ' -f2)
+    IS_SELEN_LIVE=$(curl -I -X GET http://localhost:4444/ 2>/dev/null | head -n 1 | cut -d$' ' -f2)
     if [ "$IS_SELEN_LIVE" = 200 ] ; then
         docker run --name canary --rm -ti \
         -e USER= \
@@ -21,11 +21,9 @@ do
         -e BASE_URL=https://www.ifortuna.cz \
         -e SPEC=ifortuna/prematch.js \
         -e AT_DRIVER_HOST_NAME=selen \
-        -e ELASTIC_CLUSTER=elastic:9200 \
-        --link elastic:elastic \
-        --link selen:selen \
+        -e ELASTIC_CLUSTER=https://elasticsearch:9200 \
         -v /opt/canary-tests/:/tmp/canary-tests \
-        rdpanek/canarytrace-developer:c.2.9.10
+        quay.io/canarytrace/developer:c.2.12.2
         break
     fi
     sleep 1
