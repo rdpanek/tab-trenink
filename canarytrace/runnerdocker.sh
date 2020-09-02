5cafaeb876ab4b2db36322b83b39943b30664687
Radim Daniel Pánek <rdpanek@gmail.com>
2020-09-02 19:58:12 +0200

    Update runnerdocker.sh

--- canarytrace/runnerdocker.sh
+++ canarytrace/runnerdocker.sh
@@ -5,7 +5,7 @@ docker network create canary
 
 for i in `seq 1 1`;
 do
-echo "otoceni: ${i}"
+echo "spin: ${i}"
 
 docker kill selen
 docker rm selen
@@ -16,18 +16,14 @@ do
     IS_SELEN_LIVE=$(curl -I -X GET http://localhost:4444/ 2>/dev/null | head -n 1 | cut -d$' ' -f2)
     if [ "$IS_SELEN_LIVE" = 200 ] ; then
         docker run --name canary --rm -ti \
-        -e USER= \
-        -e PASS= \
-        -e BASE_URL=https://www.ifortuna.cz \
-        -e SPEC=ifortuna/prematch.js \
+        -e SPEC=smoke.js \
         -e AT_DRIVER_HOST_NAME=selen \
-        -e ELASTIC_CLUSTER=https://elasticsearch:9200 \
-        -v /opt/canary-tests/:/tmp/canary-tests \
-        quay.io/canarytrace/developer:c.2.12.2
+        --net canary \
+        -v $(pwd):/tmp/canary-tests \
+        rdpanek/framework:1.0
         break
     fi
     sleep 1
 done
 
 sleep 1
-done
